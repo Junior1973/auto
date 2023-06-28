b329dc4623de14ea139d17a61f2a055b436dfd90
Junior1973 <pereshfrilans@mail.ru>
2023-06-28 22:59:53 +0800

    натяжка тоже на вордпресс

--- header.php
+++ header.php
@@ -13,45 +13,45 @@
   <header class="header">
     <div class="container">
       <div class="header__top">
-        <a class="logo" href="#">
-          <img class="logo__img" src="<?php bloginfo( 'template_url'); ?>./assets/images/logo.svg" alt="logo">
-        </a>
-        <a class="phone" href="tel:+380505556677">+38 (050) 555 66 77</a>
+
+        <?php the_custom_logo();?>
+
+        <a class="phone" href="tel:<?php the_field('phone-number'); ?>"><?php the_field('phone'); ?></a>
       </div>
       <div class="header__content">
         <h1 data-wow-delay=".5s" class="header__title wow animate__fadeInLeft">
-          авто из сша “под ключ”
+          <?php the_field('header');?>
         </h1>
         <h2 data-wow-delay="1s" class="header__subtitle wow animate__fadeInLeft">
-          ЗАКАЖИ СЕБЕ АВТО ИЗ США С ВЫГОДОЙ ДО 40%
+        <?php the_field('subtitle');?>
         </h2>
         <p data-wow-delay="1.5s" class="header__text wow animate__fadeInLeft">
-          Подбор, покупка, доставка, растаможка, ремонт, оформление по договору
+        <?php the_field('details');?>
         </p>
-        <a class="button" href="#">КОНСУЛЬТАЦИЯ ЭКСПЕРТА</a>
+        <a class="button" href="#"><?php the_field('button_under_title');?></a>
         <div class="social header__social">
-          <a class="social__link" href="#">
+          <a class="social__link" href="<?php the_field('telegram');?>" target="_blank">
             <svg class="test" width="26" height="26">
-              <use xlink:href="<?php bloginfo( 'template_url'); ?>./assets/images/icon/sprite.svg#instagram"></use>
+              <use xlink:href="<?php bloginfo('template_url'); ?>./assets/images/icon/sprite.svg#instagram"></use>
             </svg>
           </a>
-          <a class="social__link" href="#">
+          <a class="social__link" href="<?php the_field('telegram');?>" target="_blank">
             <svg width="25" height="19">
               <use xlink:href="<?php bloginfo( 'template_url'); ?>./assets/images/icon/sprite.svg#telegram"></use>
             </svg>
           </a>
-          <a class="social__link" href="#">
+          <a class="social__link" href="<?php the_field('telegram');?>" target="_blank">
             <svg width="26" height="26">
               <use xlink:href="<?php bloginfo( 'template_url'); ?>./assets/images/icon/sprite.svg#whatsapp"></use>
             </svg>
           </a>
-          <a class="social__link" href="#">
+          <a class="social__link" href="<?php the_field('telegram');?>" target="_blank">
             <svg width="14" height="25">
               <use xlink:href="<?php bloginfo( 'template_url'); ?>./assets/images/icon/sprite.svg#facebook"></use>
             </svg>
           </a>
         </div>
-        <img data-wow-delay="2s" class="header__images wow animate__fadeInUpBig" src="<?php bloginfo( 'template_url'); ?>./assets/images/ford-mustang.png"
+        <img data-wow-delay="2s" class="header__images wow animate__fadeInUpBig" src="<?php the_field('image_on_home'); ?>"
           alt="ford mustang">
       </div>
     </div>
--- home.php
+++ home.php
@@ -20,69 +20,16 @@ Template Name: home
       <h2 class="title">НАШИ УСЛУГИ</h2>
       <div class="services__inner">
         <div class="services__content">
+         
+          <?php the_field('service01');?>
+
           <div class="services__content-box">
-            <h6 class="services__content-title">
-              Почему ввоз авто из США?
-            </h6>
-            <div class="services__content-textbox">
-              <p class="services__content-text">
-                Мы сравнили рынки США с Европейскими и поняли, что покупка автомобиля в Америке выгоднее в несколько
-                раз, как бы
-                парадоксально это не звучало. Это вызвано продуманной логистикой, уровнем развития сервисов по
-                оцениванию состояния авто
-                и самим процессом покупки автомобиля.
-              </p>
-              <p class="services__content-text">
-                Большинство граждан США берут автомобиль в лизинг на несколько лет и все время эксплуатации сама
-                лизинговая компания
-                занимается постоянным ТО автомобиля, вследствие чего, машины из США – один из лучших выборов для
-                автолюбителей Украины.
-              </p>
-            </div>
-          </div>
-          <div class="services__content-box">
-            <h6 class="services__content-title">
-              Из-за чего такая низкая цена?
-            </h6>
-            <div class="services__content-textbox">
-              <p class="services__content-text">
-                Битые автомобили из США выкупаются с аукционов страховых компаний. На этих аукционах машина теряет
-                половину цены даже
-                из-за минимальных повреждений. Если учитывать денежные затраты, а именно выкуп, доставку, таможню и
-                ремонт, то цена
-                аналогичного по состоянию автомобиля в Украине будет выше на 35-50%, а новые будут стоить космических
-                денег.
-              </p>
-            </div>
+             <div class="services__content-textbox">
+             </div>
             <a class="button button--decor" href="#">КОНСУЛЬТАЦИЯ ЭКСПЕРТА</a>
           </div>
         </div>
-        <ol class="services__list">
-          <li data-wow-delay="1s" class="services__item wow animate__fadeInRight">
-            <p class="services__item-title">Покупка авто</p>
-            <p class="services__item-text">Подбор автомобиля и экспертная проверка</p>
-          </li>
-          <li data-wow-delay="1.2s" class="services__item wow animate__fadeInRight">
-            <p class="services__item-title">Доставка морем</p>
-            <p class="services__item-text">Расчет оптимальной стоимости доставки авто</p>
-          </li>
-          <li data-wow-delay="1.4s" class="services__item wow animate__fadeInRight">
-            <p class="services__item-title">Растаможка авто</p>
-            <p class="services__item-text">Прохождение таможенного оформления (2-3 дня)</p>
-          </li>
-          <li data-wow-delay="1.6s" class="services__item wow animate__fadeInRight">
-            <p class="services__item-title">Ремонт авто</p>
-            <p class="services__item-text">Комплексный ремонт автомобиля на СТО</p>
-          </li>
-          <li data-wow-delay="1.8s" class="services__item wow animate__fadeInRight">
-            <p class="services__item-title">Сертификация</p>
-            <p class="services__item-text">Услуга предоставляется по желанию</p>
-          </li>
-          <li data-wow-delay="2s" class="services__item wow animate__fadeInRight">
-            <p class="services__item-title">Постановка на учет</p>
-            <p class="services__item-text">Оформление автомобиля в Украине</p>
-          </li>
-        </ol>
+        <?php the_field('service02');?>
       </div>
     </div>
   </section>
@@ -91,32 +38,12 @@ Template Name: home
   <section class="benefits">
     <div class="container">
       <div class="benefits__inner">
-        <img data-wow-delay="2s" class="benefits__images wow animate__fadeInUp" src="<?php bloginfo( 'template_url'); ?>./assets/images/car.png" alt="car">
+        <img data-wow-delay="2s" class="benefits__images wow animate__fadeInUp" src="<?php the_field( 'image_on_homes'); ?>" alt="car">
         <div class="benefits__content">
           <h2 class="title benefits__title">ПОЧЕМУ МЫ?</h2>
-          <ul class="benefits__list">
-            <li class="benefits__item">
-              <p class="benefits__item-num">650</p>
-              <p class="benefits__item-title">успешно доставленных авто</p>
-              <p class="benefits__item-text">
-                большой опыт пригона автомобилей из США под ключ, все клиенты остались довольны на 100%
-              </p>
-            </li>
-            <li class="benefits__item">
-              <p class="benefits__item-num">5</p>
-              <p class="benefits__item-title">лет на рынке Украины</p>
-              <p class="benefits__item-text">
-                Работаем по всей территории Украины, работаем по договору с клиентами
-              </p>
-            </li>
-            <li class="benefits__item">
-              <p class="benefits__item-num">100%</p>
-              <p class="benefits__item-title">доверия клиентов</p>
-              <p class="benefits__item-text">
-                Онлайн отчетность. Вы всегда в курсе статуса подбора вашего авто. Фото и видео отчет
-              </p>
-            </li>
-          </ul>
+
+          <?php the_field('service03');?>
+
         </div>
       </div>
     </div>
@@ -129,40 +56,26 @@ Template Name: home
         ПРИГНАННЫЕ НАМИ АВТО
       </h2>
       <div class="carousel__inner">
-
         <?php
             global $post;
-
             $myposts = get_posts([ 
               'numberposts' => -1,
              // 'offset'      => 2,
              // 'category'    => 0
             ]);
-
             if( $myposts ){
-              foreach( $myposts as $post ){
-                setup_postdata( $post );
+            foreach( $myposts as $post ){
+            setup_postdata( $post );
         ?>
                 <!-- Вывод постов, функции цикла: the_title() и т.д. -->
             <div class="carousel__item">
               <div class="carousel__item-box">
-
-              <!-- <img class="carousel__item-img" src=" <?php bloginfo( 'template_url'); ?> ./assets/images/carousel/1.jpg" alt=""> -->
-
-              <?php the_post_thumbnail(array(380, 250), array('class' => 'carousel__item-img',)); ?>
-
+                <?php the_post_thumbnail(array(380, 250), array('class' => 'carousel__item-img',)); ?>
                 <h4 class="carousel__item-title"><?php the_title();?></h4>
                 <p class="carousel__item-text"><?php the_content(); ?></p>
               </div>
             </div>
-
         <?php } } wp_reset_postdata(); ?>
-
-
-
-
-
-
       </div>
     </div>
   </section>
@@ -180,30 +93,26 @@ Template Name: home
             <li class="contacts__item">
               <p class="contacts__item-title">Адрес</p>
               <p class="contacts__item-text">
-                Киев, Подол <br>
-                ул. Константиновская, д.71
+              <?php the_field('address'); ?>
               </p>
             </li>
             <li class="contacts__item">
               <p class="contacts__item-title">Время работы</p>
               <p class="contacts__item-text">
-                Пн-Сб: с 9:00 до 19:00, <br>
-                Вс: выходной
+              <?php the_field('schedule'); ?>
               </p>
             </li>
             <li class="contacts__item">
               <p class="contacts__item-title">Телефон</p>
               <p class="contacts__item-text">
-                +38 (050) 555 66 77
+              <a href="tel:<?php the_field('phone-number'); ?>"><?php the_field('phone'); ?></a>
               </p>
             </li>
           </ul>
         </div>
         <form class="contacts__form">
           <h2 class="title contacts__title">Оставить заявку</h2>
-          <input class="contacts__input" type="text" placeholder="Как Вас зовут?">
-          <input class="contacts__input" type="tel" placeholder="Ваш номер телефона">
-          <button class="contacts__btn button" type="submit">Отправить заявку</button>
+          <?php echo do_shortcode ('[contact-form-7 id="32" title="Контактная форма"]');?>
         </form>
       </div>
     </div>
